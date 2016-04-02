3396c6e526cbf0adc90ea248e5143a54ab062d4e
Andrew Block <andy.block@gmail.com>
2016-04-02 09:45:32 -0500

    Reordered git config

--- script/cideploy.sh
+++ script/cideploy.sh
@@ -5,13 +5,12 @@ openssl aes-256-cbc -K $encrypted_4ffc634c0a1c_key -iv $encrypted_4ffc634c0a1c_i
 eval "$(ssh-agent -s)"
 cp -f deploy_key.pem ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 ssh-keyscan playbookstest-rhtconsulting.rhcloud.com >> ~/.ssh/known_hosts
 cd _site/
+git init
 git config user.name "Travis"
 git config user.emal "noreply@redhat.com"
-git init
 git add *
 git commit -m "Deploy for ${TRAVIS_COMMIT}"
 git remote add deploy ssh://564b93450c1e666a530000d1@playbookstest-rhtconsulting.rhcloud.com/~/git/playbookstest.git/
