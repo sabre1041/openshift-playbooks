6f8aa82a1bd392d52c83b56d0f53adb4e3be5225
Andrew Block <andy.block@gmail.com>
2017-02-11 00:25:45 -0600

    Debug

--- script/cideploy.sh
+++ script/cideploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-
+env
 # Deploy site if on master branch and not PR
 if [ "$TRAVIS_REPO_SLUG" == "redhat-cop/openshift-playbooks" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == false ]; then
 
